7b17c8dd4f31fd9ae6cca66a505ef21db0400007
1011x <1011x@myopera.com>
2015-03-11 04:58:16 +0000

    Removed unused functions

--- util/MathHelper.js
+++ util/MathHelper.js
@@ -12,40 +12,4 @@ var MathHelper = {
 			s += num
 		return s
 	},
-	
-	some: function(list, expr){
-		for(var elem of list)
-			if(expr(elem))
-				return true
-		return false
-	},
-	
-	all: function(list, expr){
-		for(var elem of list)
-			if(!expr(elem))
-				return false
-		return true
-	},
-	
-	maxabs: function(list){
-		var max = 0
-		var absmax = 0
-		for(var num of list)
-			if(Math.abs(num) > absmax){
-				max = num
-				absmax = Math.abs(num)
-			}
-		return max
-	},
-	
-	minabs: function(list){
-		var min = Infinity
-		var absmin = Infinity
-		for(var num of list)
-			if(Math.abs(num) < absmin){
-				min = num
-				absmin = Math.abs(num)
-			}
-		return min
-	},
 }
