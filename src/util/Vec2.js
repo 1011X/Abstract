33bfa0cbb3dbcfbae759e992fee09c80b7d2ff8c
1011X <1011XXXXX@gmail.com>
2016-09-13 08:28:54 -0400

    Cleaned up Vec2.js

--- src/util/Vec2.js
+++ src/util/Vec2.js
@@ -1,71 +1,75 @@
 "use strict"
 
-let Vec2 = {
-	add(a, b, o = new Array(2)) {
+class Vec2 {
+	
+	static create(x, y) {
+		return new Float32Array([x, y])
+	}
+	
+	static add(a, b, o) {
 		o[0] = a[0] + b[0]
 		o[1] = a[1] + b[1]
-		return o
 	}
 	
-	subtract(a, b, o = new Array(2)) {
+	static subtract(a, b, o) {
 		o[0] = a[0] - b[0]
 		o[1] = a[1] - b[1]
-		return o
 	}
 	
-	scale(a, s, o = new Array(2)) {
+	static scale(a, s, o) {
 		o[0] = a[0] * s
 		o[1] = a[1] * s
-		return o
 	}
 	
-	dot(a, b) {
+	static dot(a, b) {
 		return a[0] * b[0] + a[1] * b[1]
 	}
 	
-	resize(a, s, o = new Array(2)) {
-		Vec2.scale(a, s / Vec2.length(a), o)
-		return o
+	static resize(a, s, o) {
+		let len = Vec2.length(a)
+		o[0] = a[0] * s / len
+		o[1] = a[1] * s / len
 	}
 
-	normalize(a, o = new Array(2)) {
-		Vec2.resize(a, 1, o)
-		return o
+	static normalize(a, o) {
+		let len = Vec2.length(a)
+		o[0] = a[0] / len
+		o[1] = a[1] / len
 	}
 
-	lengthSqr(a) {
-		return Vec2.dot(a, a)
+	static lengthSqr(a) {
+		return a[0] * a[0] + a[1] * a[1]
 	}
 	
-	length(a) {
-		return Math.sqrt(Vec2.lengthSqr(a))
+	static length(a) {
+		return Math.hypot(a[0], a[1])
 	}
 	
-	reverse(a, o = new Array(2)) {
-		Vec2.scale(a, -1, o)
-		return o
+	static reverse(a, o) {
+		o[0] = -a[0]
+		o[1] = -a[1]
 	}
 
-	rotate(a, t, o = new Array(2)) {
+	static rotate(a, t, o) {
+		/* BACKUP:
 		var temp = Vec2.copy(a)
 		var xo = Math.cos(t)
 		var yo = Math.sin(t)
 		o[0] = temp[0] * xo + temp[1] * yo
 		o[1] = temp[1] * xo - temp[0] * yo
 		return o
+		*/
+		var xo = Math.cos(t)
+		var yo = Math.sin(t)
+		o[0] = a[0] * xo + a[1] * yo
+		o[1] = a[1] * xo - a[0] * yo
 	}
 	
-	equal(a, b) {
+	static equal(a, b) {
 		return a[0] == b[0] && a[1] == b[1]
 	}
 	
-	copy(src, dst = new Array(2)) {
-		dst[0] = src[0]
-		dst[1] = src[1]
-		return dst
-	}
-
-	toString(a) {
-		return "<" + a[0] + ", " + a[1] + ">"
+	static toString(a) {
+		return `<${a[0]}, ${a[1]}>`
 	}
 }
